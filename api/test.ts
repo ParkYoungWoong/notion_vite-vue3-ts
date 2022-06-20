a3478e28f5fbfb9b48e09b388a79702899ed01f2
ParkYoungWoong <thesecon@gmail.com>
2022-06-20 16:22:42 +0900

    Vercel api 폴더 제거

--- api/test.ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default function handler(request: VercelRequest, response: VercelResponse) {
-  response
-    .status(200)
-    .json({
-      name: 'Heropy!!'
-    })
-}

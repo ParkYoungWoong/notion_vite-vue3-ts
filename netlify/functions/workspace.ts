165d5b038f8dd6be64b17a58ec225220cd230557
ParkYoungWoong <thesecon@gmail.com>
2022-07-11 20:35:50 +0900

    Add .env

--- netlify/functions/workspace.ts
+++ netlify/functions/workspace.ts
@@ -1,17 +1,25 @@
 import { Handler } from '@netlify/functions'
 import axios from 'axios'
 
-const handler: Handler = async (event, context) => {
+// process.env.APIKEY
+// process.env.USERNAME
+const { APIKEY, USERNAME } = process.env
+
+const handler: Handler = async event => {
   const { id, method, data } = JSON.parse(event.body as string)
-  await axios({
+  const { data: returnValue } = await axios({
     url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/notion/workspaces/${id}`,
     method,
     headers: {
       'content-type': 'application/json',
-      'apikey': 'FcKdtJs202204',
-      'username': 'ParkYoungWoong'
+      'apikey': APIKEY as string,
+      'username': USERNAME as string
     },
     data
   })
+  return {
+    statusCode: 200,
+    body: JSON.stringify(returnValue)
+  }
 }
 export { handler }
